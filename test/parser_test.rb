33c74a41924ef7e2bd18339d23d2982d906e4b2e
James Rosen <james@u-presence.com>
2008-06-06 14:56:20 -0400

    including Rcal::Parser to clean up scoping

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -2,14 +2,15 @@ require File.join(File.dirname(__FILE__), 'test_helper')
 require 'rcal/parser'
 
 class ParserTest < Test::Unit::TestCase
+  include Rcal::Parser
   
   def test_default_default_compliance_level_is_strict
-    assert_equal Rcal::Parser::STRICT, Rcal::Parser::default_compliance_level
+    assert_equal STRICT, Rcal::Parser::default_compliance_level
   end
   
   def test_set_default_compliance_level_to_valid_value
-    Rcal::Parser::default_compliance_level = Rcal::Parser::LAX
-    assert_equal Rcal::Parser::LAX, Rcal::Parser::default_compliance_level
+    Rcal::Parser::default_compliance_level = LAX
+    assert_equal LAX, Rcal::Parser::default_compliance_level
   end
   
   def test_set_default_compliance_level_to_invalid_value
