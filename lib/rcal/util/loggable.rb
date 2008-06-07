16601b48022f5a59416e08cf7ca4d78fba8b62a6
James Rosen <james@u-presence.com>
2008-06-06 21:59:10 -0400

    required rcal/util from Loggable so it could find the parent module

--- lib/rcal/util/loggable.rb
+++ lib/rcal/util/loggable.rb
@@ -1,52 +1,47 @@
 require 'logger'
+require 'rcal/util'
 
-module Rcal #:nodoc:
-  module Util #:nodoc:
-    
-    # Simple logging support.
-    module Loggable
-      
-      # Returns the default logger.  Unless it has been set, this logs to
-      # STDOUT, and is at level DEBUG.
-      def self.default_logger
-        @@default_logger ||= initialize_default_logger
-      end
-      
-      # Sets the default logger.
-      #
-      # Returns +nil+.
-      #
-      # Raises ArgumentError unless +logger+ is a Logger.
-      def self.default_logger=(logger)
-        raise ArgumentError.new("#{logger} is not a Logger") unless logger.kind_of?(Logger)
-        @@default_logger = logger
-        nil
-      end
-      
-      # Returns the custom logger if set, otherwise Rcal::Util::Loggable.default_logger.
-      def logger
-        @logger || Rcal::Util::Loggable.default_logger
-      end
-      
-      # Sets a custom logger.
-      #
-      # Returns +nil+.
-      #
-      # Raises ArgumentError unless +logger+ is a Logger.
-      def logger=(logger)
-        raise ArgumentError.new("#{logger} is not a Logger") unless logger.kind_of?(Logger)
-        @logger = logger
-        nil
-      end
-      
-      private
-      
-      def self.initialize_default_logger
-        result = Logger.new(STDOUT)
-        result.level = Logger::DEBUG
-        result
-      end
-    end
-    
+# Simple logging support.
+module Rcal::Util::Loggable
+  
+  # Returns the default logger.  Unless it has been set, this logs to
+  # STDOUT, and is at level DEBUG.
+  def self.default_logger
+    @@default_logger ||= initialize_default_logger
+  end
+  
+  # Sets the default logger.
+  #
+  # Returns +nil+.
+  #
+  # Raises ArgumentError unless +logger+ is a Logger.
+  def self.default_logger=(logger)
+    raise ArgumentError.new("#{logger} is not a Logger") unless logger.kind_of?(Logger)
+    @@default_logger = logger
+    nil
+  end
+  
+  # Returns the custom logger if set, otherwise Rcal::Util::Loggable.default_logger.
+  def logger
+    @logger || Rcal::Util::Loggable.default_logger
+  end
+  
+  # Sets a custom logger.
+  #
+  # Returns +nil+.
+  #
+  # Raises ArgumentError unless +logger+ is a Logger.
+  def logger=(logger)
+    raise ArgumentError.new("#{logger} is not a Logger") unless logger.kind_of?(Logger)
+    @logger = logger
+    nil
+  end
+  
+  private
+  
+  def self.initialize_default_logger
+    result = Logger.new(STDOUT)
+    result.level = Logger::DEBUG
+    result
   end
 end
