d16e60ce070924cd9ca9c57039c6e5393173f036
James Rosen <james@u-presence.com>
2008-06-07 20:25:17 -0400

    added error messages to ParserTestCase tests

--- test/lib/parser_test_case.rb
+++ test/lib/parser_test_case.rb
@@ -23,21 +23,21 @@ module ParserTestCase
   
   def assert_is_parser_for(*values)
     values.each do |v|
-      assert is_parser_for?(v)
+      assert is_parser_for?(v), "Should be parser for #{v}"
     end
   end
   
   def assert_is_wrong_parser_for(*values)
     values.each do |v|
-      assert !is_parser_for?(v)
+      assert !is_parser_for?(v), "Should not be parser for #{v}"
     end
   end
   
   def assert_parses(*values)
+    assert_is_parser_for *values
     values.each do |v|
-      assert is_parser_for?(v), "Should be able to parse #{v}" + (context.nil? ? '' : " in context #{context}")
       with_compliance_level(STRICT) do
-        assert_nothing_raised do
+        assert_nothing_raised("Should be able to parse #{v}") do
           param = parse(v)
           # TODO: implement .to_ical
           # assert_equal v, param.to_ical
@@ -49,10 +49,10 @@ module ParserTestCase
   def assert_cannot_parse(*values)
     values.each do |v|
       with_compliance_level(STRICT) do
-        assert_raises(ParseError) { parse(v) }
+        assert_raises(ParseError, "Parsing #{v} at level strict should raise a ParseError") { parse(v) }
       end
       with_compliance_level(LAX) do
-        assert_nil parse(v)
+        assert_nil parse(v), "Parsing #{v} at level lax should return nil"
       end
     end
   end
