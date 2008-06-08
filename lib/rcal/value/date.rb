d2098e9a385bb404ec3090c16511e86184a616ae
James Rosen <james@u-presence.com>
2008-06-07 20:36:22 -0400

    added DATE value and tests

---
+++ lib/rcal/value/date.rb
@@ -0,0 +1,43 @@
+require 'rcal/value/parser'
+
+class Rcal::Value::DateParser < Rcal::Value::Parser
+  
+  day = /(?:0[1-9])|(?:[1-2][0-9])|(?:3[01])/
+  month = /(?:0[1-9])|(?:1[0-2])/
+  year = /[0-9]{4}/
+
+  # From {RFC 2445 4.3.4}[link:/files/doc/RFC_2445_rdoc.html]:
+  #   date               = date-value
+  #
+  #   date-value         = date-fullyear date-month date-mday
+  #   date-fullyear      = 4DIGIT
+  #   date-month         = 2DIGIT        ;01-12
+  #   date-mday          = 2DIGIT        ;01-28, 01-29, 01-30, 01-31
+  #                                      ;based on month/year
+  #
+  # Matchdata:
+  # 0. Whole date string
+  # 1. Year
+  # 2. Month
+  # 3. Day
+  DATE = Regexp.new("(#{year.source})(#{month.source})(#{day.source})")
+  
+  @@value_type = 'DATE'
+  cattr_reader :value_type
+  
+  # Returns +true+ iff +ical+ is an Ical Date.
+  def is_parser_for?(ical)
+    ical =~ DATE
+  end
+  
+  # Returns a Ruby Date.
+  def parse(ical, context)
+    return wrong_parser!(ical, context, "#{ical} is not a Date") unless is_parser_for?(ical)
+    begin
+      Date.civil(*ical.match(DATE)[1,3].map { |s| s.to_i })
+    rescue ArgumentError
+      error!(ical, context, "#{ical} is not a valid Date")
+    end
+  end
+  
+end
---
+++ test/value/date_test.rb
@@ -0,0 +1,43 @@
+require File.join(File.dirname(__FILE__), '..', 'test_helper')
+require 'rcal/value/date'
+require 'parser_test_case'
+require 'date'
+
+class DateTest < Test::Unit::TestCase
+  include ParserTestCase
+  
+  def setup
+    @parser = Rcal::Value::DateParser.new
+  end
+  
+  def test_value_type_is_BOOLEAN
+    assert_equal 'DATE', @parser.value_type
+  end
+  
+  def test_is_parser_for_dates
+    assert_is_parser_for '20020719', '19991231'
+  end
+  
+  def test_is_not_parser_for_non_ical_dates
+    assert_is_wrong_parser_for '2005', '1565012', 'December 3, 2000'
+  end
+  
+  def test_error_parsing_invalid_date
+    assert_cannot_parse '20000631', '20070431', '20050229'
+  end
+  
+  def test_parses_dates
+    assert_parses '19120224', '17130808', '23000101'
+  end
+  
+  def test_parses_to_ruby_dates
+    ['19120224', '17130808', '23000101'].each do |d|
+      assert parse(d).kind_of?(Date)
+    end
+  end
+  
+  def test_parses_to_correct_date
+    assert_equal '1854-09-10', parse('18540910').strftime('%Y-%m-%d')
+  end
+  
+end
