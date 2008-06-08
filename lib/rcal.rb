79ce5ae319946434d96492fda7e6a6c6affc94db
James Rosen <james@u-presence.com>
2008-06-08 10:40:16 -0400

    removed extra parentheses

--- lib/rcal.rb
+++ lib/rcal.rb
@@ -8,7 +8,7 @@
 # Properties, and Parameters.  These are mapped to modules
 # (Component[link:/classes/Rcal/Component.html],
 # Property[link:/classes/Rcal/Property.html],
-# Parameter[link:/class/rcal/Parameter.html]),
+# Parameter[link:/class/rcal/Parameter.html],
 # and Value[link:/class/rcal/Value.html]) in Rcal.
 # Components, Proprerties, and Parameters are collectively called
 # "parts" in this documentation (see Part[link:/classes/Rcal/Part.html]).
@@ -20,7 +20,7 @@
 # method and a +to_ical+ instance method.  Each of the
 # Component[link:/classes/Rcal/Component.html],
 # Property[link:/classes/Rcal/Property.html],
-# Parameter[link:/class/rcal/Parameter.html]),
+# Parameter[link:/class/rcal/Parameter.html],
 # and Value[link:/class/rcal/Value.html]) modules themselves has
 # a <tt>parser(compliance_level)</tt> class method that returns a registry
 # of all of the known individual parsers.  Users of the library can add
